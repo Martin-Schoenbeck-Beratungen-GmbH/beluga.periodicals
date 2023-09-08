9a1a34258a53752f1f1f46ada1a623040216d5e3
Lukas <lukas@schoenbeck.de>
2023-09-08 14:58:31 +0200

    test suite prepared
    prepared test suite and specific event handler tests

---
+++ test/de/schoenbeck/periodicals/event/EventHandlerTests.java
@@ -0,0 +1,40 @@
+package de.schoenbeck.periodicals.event;
+
+import org.idempiere.test.AbstractTestCase;
+import org.junit.jupiter.api.BeforeEach;
+
+public class EventHandlerTests extends AbstractTestCase {
+
+	/**
+	 * Prepare config here because each test gets their own transaction.
+	 */
+	@BeforeEach
+	public void prepare () {
+		//TODO: create periodical config
+		//TODO: two different periodical subscriptions
+	}
+	
+	public void createSubscriber_SingleOrderLine () {
+		//TODO
+	}
+	
+	public void createNoSubscriber_SingleOrderLine () {
+		//TODO
+	}
+	
+	public void createOneSubscriber_MultipleOrderLines () {
+		//TODO
+	}
+	
+	public void createOneSubscriber_MultipleOrderLines_SamePeriodicalDifferentProduct () {
+		//TODO
+	}
+	
+	public void createTwoSubscribers_MultipleOrderLines () {
+		//TODO
+	}
+	
+	public void createNoSubscriber_ReusedOrder () {
+		//TODO
+	}
+}
---
+++ test/de/schoenbeck/periodicals/process/ProcessTests.java
@@ -0,0 +1,8 @@
+package de.schoenbeck.periodicals.process;
+
+import org.idempiere.test.AbstractTestCase;
+
+public class ProcessTests extends AbstractTestCase {
+
+	 
+}
